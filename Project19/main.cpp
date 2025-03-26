612f6c23606c98a48898d79558928bee8e085dac
yongjae410 <yongjae410@gmail.com>
2025-03-26 17:10:08 +0900

    add Cal::getGop()

--- Project19/main.cpp
+++ Project19/main.cpp
@@ -2,11 +2,22 @@
 
 class Cal {
 public :
-
+	int getGop(int a, int b)
+	{
+		return a * b;
+	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(CalTest, getGopTest)
+{
+	Cal myCal;
+	EXPECT_EQ(myCal.getGop(1, 2), 2);
+	EXPECT_EQ(myCal.getGop(0, 3), 0);
+	EXPECT_EQ(myCal.getGop(-1, -1), 1);
 }
 
 int main() {
