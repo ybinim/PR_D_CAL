7f2d90a0b08eaaf9b38fcfc27de9d633a53ea844
yj00114.jang <yjj00114@gmail.com>
2025-03-26 17:16:41 +0900

    add getMinus

--- Project19/main.cpp
+++ Project19/main.cpp
@@ -2,11 +2,22 @@
 
 class Cal {
 public :
-
+	int getMinus(int a, int b)
+	{
+		return a - b;
+	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(CalTest, getMinusTest)
+{
+	Cal testCal;
+	EXPECT_EQ(testCal.getMinus(1, 2), -1);
+	EXPECT_EQ(testCal.getMinus(3, 0), 3);
+	EXPECT_EQ(testCal.getMinus(-1, -1), 0);
 }
 
 int main() {
