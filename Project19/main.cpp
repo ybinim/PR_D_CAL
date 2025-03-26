e54c1f463b76c3f333aea81c00effe7bead95e6d
Yina Yoon <dlsk030303@naver.com>
2025-03-26 17:11:26 +0900

    add getSumSum()

--- Project19/main.cpp
+++ Project19/main.cpp
@@ -2,11 +2,20 @@
 
 class Cal {
 public :
-
+	int getSumSum(int a, int b, int c) {
+		return (a + b + c);
+	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, testSumSum) {
+	int expected = 6;
+	Cal cal;
+	int actual = cal.getSumSum(1, 2, 3);
+	EXPECT_EQ(expected, actual);
 }
 
 int main() {
